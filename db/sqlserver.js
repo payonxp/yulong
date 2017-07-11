398cf4b650f2c641e11388530eb0880731879d00
payonxp <522983486@qq.com>
2017-07-11 15:07:38 +0800

    add sqlserver module

---
+++ db/sqlserver.js
@@ -0,0 +1,65 @@
+/**
+ * Created by tpeng on 2017/7/11.
+ */
+let sql = require('msnodesqlv8');
+let fs = require('fs');
+let dbConnector = {};
+
+// for json bom
+// var buf = fs.readFileSync('db.json');
+// fs.writeFileSync('db.json', buf.slice(3));
+
+let str = fs.readFileSync('db.json');
+let obj = JSON.parse(str);
+
+if (obj.connStr === null) {
+    console.log("no connection string");
+}
+let connStr = obj.connStr;
+sql.open(connStr, function (err, conn) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("sqlserver connect success.");
+        dbConnector.query = function (q, cb) {
+            conn.query(q, function (err, res) {
+                if (err) {
+                    console.log(err);
+                }
+                cb(res);
+            });
+        };
+
+        dbConnector.preparedQuery = function (q, p, cb) {
+            conn.query(q, p, function (err, res) {
+                if (err) {
+                    console.log(err);
+                }
+                cb(res);
+            });
+        };
+
+        /* NOTICE:
+         * for callback param cb(result, output)
+         * the sp return value will always be the first member of output vector
+         * and each output parameter in order will then appear
+         */
+        dbConnector.execSP = function (sp, p, cb) {
+            let pm = conn.procedureMgr();
+            pm.callproc(sp, p, function (err, res, output) {
+                if (err) {
+                    console.log(err);
+                }
+                cb(res, output);
+            });
+        };
+
+        dbConnector.describeSP = function (sp, cb) {
+            let pm = conn.procedureMgr();
+            pm.describe(sp, cb);
+        };
+    }
+});
+
+module.exports = dbConnector;
+
